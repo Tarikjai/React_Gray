ef5048ee43ba847f8a590828229fcd95bd859338
Tarik <tarik.jaidani@gmail.com>
2024-10-29 22:33:04 +0100

    component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
-import logo from './logo.svg';
+import Content from './Content';
 import './App.css';
+import Header from './Header'
+import Footer from './Footer'
 
 function App() {
-  const handleNameChange = ()=>{
-    const names =["Tarik","Louison","Samantha"]
-    const int = Math.floor(Math.random()*3)
-    return names[int]
-  }
-   
-
   return (
    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello {handleNameChange()}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header />
+     <Content />
+     <Footer />
     </div>
   );
 }
